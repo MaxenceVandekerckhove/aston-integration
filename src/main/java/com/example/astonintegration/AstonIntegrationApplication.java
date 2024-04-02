e46c355a5f8ae1b49eb6c55737aaf53d537452ce
MaxenceVandekerckhove <maxence.vandekerckhove@viacesi.fr>
2024-04-02 09:47:13 +0200

    initial commit

---
+++ src/main/java/com/example/astonintegration/AstonIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.astonintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstonIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstonIntegrationApplication.class, args);
+    }
+
+}
