a05f5b9febebcb5998e8786c467881331670fba4
MaxenceVandekerckhove <maxence.vandekerckhove@viacesi.fr>
2024-04-30 14:18:20 +0200

    feat(test): Add home controller

--- src/main/java/com/example/astonintegration/controller/Home.java
+++ src/main/java/com/example/astonintegration/controller/Home.java
@@ -1,9 +1,14 @@
 package com.example.astonintegration.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Home {
 
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
 
 }
